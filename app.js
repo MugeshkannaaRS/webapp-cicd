e333f75fb65530ddeb1dd271e307bf03a40d24d8
MugeshkannaaRS <115528051+MugeshkannaaRS@users.noreply.github.com>
2025-11-01 14:46:10 +0530

    code app

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
 
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello from AWS CI/CD Pipeline using Jenkins & Docker!\n');
+});
+
+server.listen(8080, () => {
+  console.log('Server running on port 8080');
+});
